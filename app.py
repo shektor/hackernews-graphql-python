4c1aba8a2235848b0cb54bcfa007eacc6fa8e89f
shektor <s.hektor@gmail.com>
2020-07-14 16:48:20 +0100

    Add graphql endpoint

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+from database import db_session, init_db
+from schema import schema
+
 from flask import Flask
+from flask_graphql import GraphQLView
 
 
 app = Flask(__name__)
@@ -6,3 +10,17 @@ app = Flask(__name__)
 @app.route('/ping')
 def ping():
     return 'pong'
+
+app.add_url_rule(
+    "/graphql",
+    view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
+)
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+init_db()
+# if __name__ == "__main__":
+    # init_db()
+    # app.run()
